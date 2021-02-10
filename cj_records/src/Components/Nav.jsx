1b67b02df220ffc631fdeaec976746ecea9d456e
Christopher J <chrisbjones1987@yahoo.com>
2021-02-10 14:50:31 -0600

    Moved product_images folder, ReVamped Logo & Navigation Bar, Got Record Images to display on the products page

--- cj_records/src/Components/Nav.jsx
+++ cj_records/src/Components/Nav.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
+import logo from './logo.png';
 
 function Nav() {
     return (
-        <nav>
-            <h3>Logo</h3>
+        <nav className="nav-bar">
+            <img className="logo" src={logo} />
             <ul className="nav-links">
                 <Link to="/"><li>Home</li></Link>
                 <Link to="/products"><li>Products</li></Link>
--- cj_records/src/Views/Products.jsx
+++ cj_records/src/Views/Products.jsx
@@ -28,12 +28,18 @@ function Products() {
 
     return (
         <div>
-            <h1>Records</h1>
+            <h1>Products Page</h1>
             {myJson.map((record) => (
                 <div key={record.id}>
                     <p>{record.artist}</p>
                     <p>{record.album}</p>
-                    <p>{record.front_cover_image}</p>
+                    <p>{record.genre}</p>
+                    <p>${record.price}.00</p>
+                    <p>{record.label}</p>
+                    <br/>
+                    <img src={record.front_cover_image} alt="front cover"/>
+                    <br/>
+                    <img src={record.back_cover_image} alt="back cover"/>
                     <hr/>
                 </div>
             ))}
