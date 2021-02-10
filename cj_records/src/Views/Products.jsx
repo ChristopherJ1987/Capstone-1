42424ba4c11aa37c2c78f325b7edcb4a676ad3c1
Christopher J <chrisbjones1987@yahoo.com>
2021-02-09 23:00:48 -0600

    Added Records to the Products Page

--- cj_records/src/Views/Products.jsx
+++ cj_records/src/Views/Products.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../App.css';
+import Records from '../products.json';
 
 function Products() {
+
+    const myJson = Records;
+    
+    const getData = () => {
+        fetch('products.json', {
+            headers: {
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json'
+            }
+        })
+        .then(function(response) {
+            console.log(response)
+            return response.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+        });
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <div>
-            <h1>Products Page</h1>
+            <h1>Records</h1>
+            {myJson.map((record) => (
+                <div key={record.id}>
+                    <p>{record.artist}</p>
+                    <p>{record.album}</p>
+                    <p>{record.front_cover_image}</p>
+                    <hr/>
+                </div>
+            ))}
         </div>
     )
 }
